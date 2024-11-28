023828d26b7abf488dc0c07b392cce280a2f05fd
Han Minju <hans200322@gmail.com>
2024-11-29 01:40:36 +0900

    2024년 11월 29일 01번

---
+++ cmake-build-debug/CMakeFiles/1015.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "1015"
+  "1015.pdb"
+  "CMakeFiles/1015.dir/main.c.o"
+  "CMakeFiles/1015.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1015.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
